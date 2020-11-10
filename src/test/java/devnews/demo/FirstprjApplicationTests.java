9f9713dff1cd547d41a7f49c197c1b45739a2c54
LenaRay <photoinstockholm@gmail.com>
2020-11-10 15:37:54 +0100

    initial set up of the project

---
+++ src/test/java/devnews/demo/FirstprjApplicationTests.java
@@ -0,0 +1,13 @@
+package devnews.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
